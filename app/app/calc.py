d67b334f0022938f6219164a19352a07945063be
mariana299 <marianadassis299@hotmail.com>
2022-12-09 19:56:18 +0100

    checks

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,10 +1,12 @@
-""" 
-Calculator
+"""
+Calculator functions
 """
 
-def add(x ,y):
+
+def add(x, y):
     """Add x and y and return result."""
     return x + y
+
 
 def subtract(x, y):
     """Subtract x from y and return result."""
